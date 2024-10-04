bde0d4662c9ed1ae952d6d21dffc02875e00b654
Dang Trung Hieu <dangtrunghieu0904hcm@gmail.com>
2024-10-04 09:30:34 +0700

    merge 2 branch

---
+++ src/components/Skeleton/SkeletionIndexPage/SkeletonComponent.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SkeletonComponent = () => {
+  return (
+    <div>
+      <div class="relative animate-pulse">
+        <div class="h-[340px] sm:h-[260px] md:h-[350px] lg:h-[258px] bg-gray-200 rounded-xl"></div>
+        <div class="mt-3">
+          <div class="h-4 bg-gray-200 rounded w-3/4"></div>
+          <div class="mt-2 h-4 bg-gray-200 rounded w-1/2"></div>
+          <div class="mt-2 h-4 bg-gray-200 rounded w-full"></div>
+        </div>
+        <div class="absolute top-[10px] right-[10px] p-[10px] rounded-full bg-gray-300"></div>
+      </div>
+    </div>
+  );
+};
+
+export default SkeletonComponent;
--- src/hooks/useRoutesCustome.jsx
+++ src/hooks/useRoutesCustome.jsx
@@ -8,6 +8,45 @@ import UserManagePage from '../pages/UserManagePage/UserManagePage.jsx';
 import LocationManagePage from '../pages/LocationManagePage/LocationManagePage.jsx';
 import RoomInfoManagePage from '../pages/RoomInfoManagePage/RoomInfoManagePage.jsx';
 import { path } from '../common/path/path.js';
+import DelayedRender from "../components/DelayedRender/DelayedRender";
+import { Skeleton } from "antd";
+import SkeletonIndexPage from "../components/Skeleton/SkeletionIndexPage/SkeletonIndexPage";
+import SkeletonLeftListRoom from "../components/Skeleton/SkeletonListRoomPage/SkeletonLeftListRoom";
+import SkeletonDetailPage from "../components/Skeleton/SkeletonDetailPage/SkeletonDetailPage";
+
+// Hàm helper để trì hoãn import
+const lazyWithDelay = (importFunc, delay) => {
+    return React.lazy(() => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(importFunc());
+        }, delay);
+      });
+    });
+  };
+  
+  // Lazy load các trang với trì hoãn
+const IndexPage = lazyWithDelay(
+    () => import("../pages/IndexPage/IndexPage"),
+    2000
+);
+const ListRoomPage = lazyWithDelay(
+    () => import("../pages/ListRoomPage/ListRoomPage"),
+    2000
+);
+const RoomDetailPage = lazyWithDelay(
+    () => import("../pages/RoomDetailPage/RoomDetailPage"),
+    2000
+);
+const SignInPage = lazyWithDelay(
+    () => import("../pages/SignInPage/SignInPage"),
+    500
+);
+const SignUpPage = lazyWithDelay(
+    () => import("../pages/SignUpPage/SignUpPage"),
+    500
+);
+  
 
 const useRoutesCustome = () => {
     const routes = useRoutes([
