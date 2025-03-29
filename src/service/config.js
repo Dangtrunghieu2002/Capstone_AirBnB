4c9f03f97cbeb731a2dcb4efb6a0c326d6162bab
letrunghungprovip123 <138652865+letrunghungprovip123@users.noreply.github.com>
2025-03-29 13:18:07 +0700

    Update config.js

--- src/service/config.js
+++ src/service/config.js
@@ -4,6 +4,6 @@ export const http = axios.create({
   timeout: 30000,
   headers: {
     tokenCyberSoft:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCBTw6FuZyAxMiIsIkhldEhhblN0cmluZyI6IjA4LzAxLzIwMjUiLCJIZXRIYW5UaW1lIjoiMTczNjI5NDQwMDAwMCIsIm5iZiI6MTcxMjk0MTIwMCwiZXhwIjoxNzM2NDQyMDAwfQ.dTEJFBH9VnWoG3lE6KU86OTAeY78oRLVFwIiQgbKkCM",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJOb2RlanMgQWR2IDAzIiwiSGV0SGFuU3RyaW5nIjoiMjAvMDgvMjAyNSIsIkhldEhhblRpbWUiOiIxNzU1NjQ4MDAwMDAwIiwibmJmIjoxNzM1NTc4MDAwLCJleHAiOjE3NTU3OTU2MDB9.zhd-TJO0gE9p4RukPjsPXrnUxCFpIwyJx4Yw3fVyVas",
   },
 });
